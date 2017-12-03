ec8de57da54e58385a32e9352c6b1a0adf3eea1d
alay <contacto@adappgeo.net>
2017-12-03 18:50:38 +0100

    Reportaje sonoro candidato

---
+++ js/audiolist.js
@@ -0,0 +1,15 @@
+var ap1 = new APlayer({
+    element: document.getElementById('DondeCruzanCaminos'),
+    narrow: false,
+    autoplay: false,
+    showlrc: false,
+    mutex: true,
+    theme: '#e6d0b2',
+    mode: 'circulation',
+    music: {
+        title: 'Donde se cruzan los caminos',
+        author: 'Gonzalo Estefanía, Gestiona Radio',
+        url: '../mp3/DondeCruzanCaminos.mp3',
+        pic: '../mp3/GestionaRadio.png'
+    }
+});
